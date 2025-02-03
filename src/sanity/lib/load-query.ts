89c450ce027a8493fefb1b95235c1689cce1e34a
irina <irina@sanity.io>
2025-02-03 16:17:58 -0500

    test

--- src/sanity/lib/load-query.ts
+++ src/sanity/lib/load-query.ts
@@ -4,7 +4,7 @@ import { sanityClient } from "sanity:client";
 const visualEditingEnabled =
   import.meta.env.PUBLIC_SANITY_VISUAL_EDITING_ENABLED === "true";
 const token = import.meta.env.SANITY_API_READ_TOKEN;
-
+console.log('token', token)
 export async function loadQuery<QueryResponse>({
     query,
     params,
